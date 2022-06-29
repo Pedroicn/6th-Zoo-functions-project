befb1055ca36e3507a0eaa11a4eabe8700abece2
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-29 16:15:22 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const getAnimal = species.find((specie) => specie.name === animal);
+  const ages = getAnimal.residents.map((element) => element.age);
+  return ages.every((element) => element >= age);
 }
 
 module.exports = getAnimalsOlderThan;
