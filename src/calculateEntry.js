fd2b06aec6a1c4be4220912b1eed4e8f1bcee99a
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-30 16:21:21 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui.
+  const entrantsObj = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      entrantsObj.child += 1;
+    } else if (element.age < 50) {
+      entrantsObj.adult += 1;
+    } else { entrantsObj.senior += 1; }
+  });
+  return entrantsObj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const arrayPrices = Object.entries(countEntrants(entrants));
+  return arrayPrices.reduce((result, current) => (result + prices[current[0]] * current[1]), 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
