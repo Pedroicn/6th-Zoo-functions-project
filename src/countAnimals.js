8659a394e196de18d24aaad723c5cdcdbe599e21
Pedro Nunes <pedro.icn88@gmail.com>
2022-06-30 11:57:40 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+function count(animal) {
+  if (animal.specie && animal.sex) {
+    const getAnimal = species.find((item) => item.name === animal.specie);
+    return getAnimal.residents
+      .reduce((acc, crr) => ((crr.sex === animal.sex) ? acc + 1 : acc), 0);
+  }
+  return species.find((item) => item.name === animal.specie).residents.length;
+}
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (!animal) {
+    species.forEach((item) => {
+      obj[item.name] = item.residents.length;
+    });
+    return obj;
+  }
+  return count(animal);
 }
 
 module.exports = countAnimals;
