8c5081306103eff9de403813bdfc7754e02305c1
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-01 15:53:10 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaborador = employees.find((employee) => employee.id === id);
+  const firstAnimal = species.find((specie) => specie.id === colaborador.responsibleFor[0]);
+  const animalCharacteristics = firstAnimal.residents
+    .reduce((acc, atual) => ((acc.age > atual.age) ? acc : atual));
+  return Object.values(animalCharacteristics);
 }
 
 module.exports = getOldestFromFirstSpecies;
