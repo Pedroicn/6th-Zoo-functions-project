9f5f82401c5d69cc0b2e8f9f6c43b3e5cfa0db8f
Pedro Nunes <pedro.icn88@gmail.com>
2022-07-01 15:02:57 -0300

    Requisito 6

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
+// const { species } = data;
 function getSchedule(scheduleTarget) {
   // const animals = species.map((specie) => specie.name);
   // if (animals.includes(scheduleTarget)) {
